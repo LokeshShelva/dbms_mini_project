4de54d8e8862853b84c1d5a4fb2532a714c43c83
Barathraj MS <msbarathraj@gmail.com>
2021-11-12 17:17:24 +0530

    commiting stash

--- react/src/App.js
+++ react/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Routes, Router } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 // import Home from "./components/Home";
+import StudentHome from "./components/StudentHome/StudentHome";
 import FacultyPage from "./components/Admin/FacultyPage/FacultyPage";
 import LoginPage from "./components/Login/LoginPage";
 import StudentPage from "./components/Admin/StudentPage/StudentPage"
@@ -38,6 +39,8 @@ class App extends Component {
                         <Route exact path="faculty" element={<FacultyPage role={this.state.token !== null ? this.state.token.role : null} />} />
                         <Route exact path="students" element={<StudentPage role={this.state.token !== null ? this.state.token.role : null} />} />
                     </Route>
+                    <Route exact path="/student_result" element = {<StudentHome />} />
+
                     {/* <Route exact path="/students" element={<Home />} />
                     <Route exact path="/result" element={<Home />} />
                     <Route exact path="/fee" element={<Home />} />
--- react/src/components/StudentHome/StudentHome.js
+++ react/src/components/StudentHome/StudentHome.js
@@ -1,20 +1,44 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button, TextField, Typography } from '@mui/material';
+import axios from "axios";
+import "./StudentHome.css";
+import { width } from "@mui/system";
 
-// function StudentNameCard() {
+function StudentHome({ userId }) {
 
-// }
+    // useEffect(
+    //     () => {
+    //         axios.get(`/api/student/userId/${userId}`).then((val) => console.log(val.data))
+    //     }, [])
 
-// function StudentHome({ userId }) {
+    return (
+        <div>
+            <div className="titleContainer">
+                <Typography variant="h3" className="title" color="primary">STUDENT DASHBOARD</Typography>
+            </div>
+            <div class="content">
+                <div class="contentLeft">
+                    <div className="studentCard">
+                        <Typography variant="h5" color="primary"  className ="name">Lokesh Selva</Typography>
+                        <Typography variant="subtitle1" fontWeight="500" color="primary" className="name">CS19B1034</Typography>
+                    </div>
+                    <div className="studentDetail">
+                        <Typography variant="subtitle2" color="primary" className="question">DOB:<span className="answer">10.10.2001</span></Typography>
+                        <Typography variant="subtitle2" color="primary" className="question">Address:<span className="answer">16, Vettaikaran street,Thalatheru Karaikal</span></Typography>
+                        <Typography variant="subtitle2" color="primary" className="question">Blood Group:<span className="answer">O+ve</span></Typography>
+                        <Typography variant="subtitle2" color="primary" className="question">Contact:<span className="answer">9874563210</span></Typography>
+                        <Typography variant="subtitle2" color="primary" className="question">DOB:<span className="answer">10.10.2001</span></Typography>
+                    </div>
+                </div>
+                <div className="contentRight">
 
-//     useEffect(
-//         () => {
-//             axios.get(`/api/student/userId/${userId}`).then((val) => console.log(val.data))
-//         }, [])
+                </div>
+            </div>
+        </div>
+        // <div style={{"width": "100%", "height": "100%", "display": "flex", "justifyContent": "center"}}>
+        //     <Typography variant="h3" className="title" color="primary">STUDENT DASHBOARD</Typography>
+        // </div>        
+    )
+}
 
-//     return (
-//         <h1>Hello</h1>
-//     )
-// }
-
-// export default StudentHome;
+export default StudentHome;
